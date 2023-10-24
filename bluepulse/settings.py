5746eddda1b7f751cfce576121de4cfc00f8c29d
Paul-Gleeson <Shagamatula@gmail.com>
2023-10-24 17:12:30 +0000

    fix: Attempt to resolve CSS loading issue on Heroku

--- bluepulse/settings.py
+++ bluepulse/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'bluepulse.urls'
